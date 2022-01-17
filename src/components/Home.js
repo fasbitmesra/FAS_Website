b2e99c419cde3611da10ef96d03f4dacac47273c
Utkarsh <singhutkarsh902@gmail.com>
2022-01-18 02:54:29 +0530

    Fixed Hamburger bug, Added home background

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,18 @@ import Events from "./homepage/Events";
 import Team from "./homepage/Team";
 import Contact from "./homepage/Contact";
 import Footer from "./Footer";
+import "./Home.scss";
+import HomeBG from "../assets/backgrounds/Home-bg.png";
 
 function Home() {
   return (
     <>
       <Navbar />
+
+      <div className="Hero">
+        <img src={HomeBG} alt="..." />
+      </div>
+
       <About />
       <Events />
       <Team />
