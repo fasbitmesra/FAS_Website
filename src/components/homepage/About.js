42578ebee03277cfd13919c75092dad6685c315f
Utkarsh <singhutkarsh902@gmail.com>
2022-01-18 16:04:03 +0530

    Added responsive About component, improved Navbar transparency

--- src/components/homepage/About.js
+++ src/components/homepage/About.js
@@ -1,9 +1,35 @@
 import React from "react";
+import FasLogo from "../../assets/elements/FAS_logo(black).png";
+
+const AboutSection = (props) => {
+  return (
+    <>
+      <img src={FasLogo} alt="FAS Logo" />
+      <div className="About-text">
+        <h2>{props.title}</h2>
+        <p>{props.desc}</p>
+      </div>
+    </>
+  );
+};
 
 function About() {
   return (
     <>
-      <h1>About</h1>
+      <div className="About-container">
+        <div className="About-section">
+          {AboutSection({
+            title: "Who Are We?",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+          })}
+        </div>
+        <div className="About-section" id="About-reverse">
+          {AboutSection({
+            title: "What We Do.",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+          })}
+        </div>
+      </div>
     </>
   );
 }
