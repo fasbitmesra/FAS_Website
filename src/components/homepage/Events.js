7b3161968a8df317831fb03c72310443e51d89e8
Utkarsh <singhutkarsh902@gmail.com>
2022-01-20 00:51:08 +0530

    Added Spectra section

--- src/components/homepage/Events.js
+++ src/components/homepage/Events.js
@@ -1,6 +1,7 @@
 import React from "react";
 import EventInfo from "../../api/EventInfo";
-import Background from "../../assets/backgrounds/Events-bg.png";
+import EventBG from "../../assets/backgrounds/Events-bg.png";
+import { NavLink } from "react-router-dom";
 
 function Events() {
   return (
@@ -19,7 +20,24 @@ function Events() {
           </div>
         </div>
       </div>
-      <img className="Events-bg" src={Background} alt="Background" />
+      <img className="Events-bg" src={EventBG} alt="Background" />
+
+      <div className="Spectra-section">
+        <div className="Spectra-text">
+          <h2>Spectra</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </p>
+          <NavLink to="/spectra">Spectra Page</NavLink>
+        </div>
+
+        <img src="" alt="..." />
+      </div>
     </>
   );
 }
