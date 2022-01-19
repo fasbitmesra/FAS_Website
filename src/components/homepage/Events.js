7e0d3b9271c4fba90511dc1aca08793d1f2f584c
Utkarsh <singhutkarsh902@gmail.com>
2022-01-19 23:58:57 +0530

    Added Events component, added api directory

--- src/components/homepage/Events.js
+++ src/components/homepage/Events.js
@@ -1,9 +1,25 @@
 import React from "react";
+import EventInfo from "../../api/EventInfo";
+import Background from "../../assets/backgrounds/Events-bg.png";
 
 function Events() {
   return (
     <>
-      <h1>Events</h1>
+      <div className="Events-container">
+        <h1>Events</h1>
+
+        <div className="Events-grid">
+          <div className="Events-row">
+            {EventInfo.map((event, index) => (
+              <div className="Events-card">
+                <img src={event.image} alt={event.title} />
+                <p>{event.desc}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <img className="Events-bg" src={Background} alt="Background" />
     </>
   );
 }
