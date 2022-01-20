bcc81cb1b6b30faa7e49bb6aaa70b5b18c1a6ce7
Utkarsh <singhutkarsh902@gmail.com>
2022-01-20 20:25:59 +0530

    Added Footer component, added FasSocialInfo api

---
+++ src/api/FasSocialInfo.js
@@ -0,0 +1,31 @@
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedinIn,
+  FaYoutube,
+} from "react-icons/fa";
+
+const FasSocialInfo = [
+  {
+    name: "Facebook",
+    link: "https://www.facebook.com/fasbitmesra",
+    icon: <FaFacebookF />,
+  },
+  {
+    name: "Instagram",
+    link: "https://www.instagram.com/fasbitmesra/",
+    icon: <FaInstagram />,
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/company/fas-bitmesra",
+    icon: <FaLinkedinIn />,
+  },
+  {
+    name: "Youtube",
+    link: "https://www.youtube.com/channel/UCB_YXFQWINIHPV1s3IBtiKQ",
+    icon: <FaYoutube />,
+  },
+];
+
+export default FasSocialInfo;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,62 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import FasSocialInfo from "../api/FasSocialInfo";
+import "./Footer.scss";
 
 function Footer() {
   return (
     <>
-      <h1>Footer</h1>
+      <div className="Footer-bg">
+        <div className="Footer-conatiner">
+          <div className="Footer-head">
+            <h1>Fine Arts Society</h1>
+            <h3>BIT Mesra</h3>
+          </div>
+          <div className="Footer-menu">
+            <h3>Menu</h3>
+            <div className="Footer-menu-col">
+              <ul className="Footer-links">
+                <li>
+                  <NavLink to="/">Home</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/about">About</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/team">Team</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/contact">Contact</NavLink>
+                </li>
+              </ul>
+              <ul className="Footer-links">
+                <li>
+                  <NavLink to="/events">Events</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/gallery">Gallery</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/spectra">Spectra</NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="Footer-social">
+            <h3>Social Media</h3>
+            <div className="Social-icon-container">
+              {FasSocialInfo.map((social, index) => {
+                const { link, icon } = social;
+                return (
+                  <a href={link} target="_blank" rel="noopener noreferrer">
+                    <div className="Social-icon">{icon}</div>
+                  </a>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
