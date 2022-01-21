615e5899f055ce5bb86713e7144120fe6b3c7a4f
Utkarsh <singhutkarsh902@gmail.com>
2022-01-21 19:22:32 +0530

    Added Team component, added TeamInfo api

--- src/components/homepage/Team.js
+++ src/components/homepage/Team.js
@@ -1,9 +1,34 @@
 import React from "react";
+import TeamInfo from "../../api/TeamInfo";
+import { ReactComponent as TeamBG } from "../../assets/backgrounds/Team-bg.svg";
 
 function Team() {
   return (
     <>
-      <h1>Team</h1>
+      <div className="Team-container">
+        <h1>Team</h1>
+        <div className="Team-faculty">
+          <div className="Faculty-card">
+            <img src={TeamInfo.faculty.image} alt="..." />
+            <TeamBG />
+          </div>
+          <p>{TeamInfo.faculty.name}</p>
+          <p className="post">{TeamInfo.faculty.post}</p>
+        </div>
+
+        <div className="Team-grid">
+          {TeamInfo.members.map((member, index) => (
+            <div className="Member-card">
+              <div className={`Member-pic col-${index % 5}`}>
+                <img src={member.image} alt="..." />
+                <TeamBG />
+              </div>
+              <p>{member.name}</p>
+              <p className="post">{member.post}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
