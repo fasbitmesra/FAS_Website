f0699b86258d2c8374310f7024974d1393eb530f
Utkarsh <singhutkarsh902@gmail.com>
2022-01-27 17:59:20 +0530

    Added Spectra Teaser

--- src/components/Spectra.js
+++ src/components/Spectra.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import "./Spectra.scss";
 
 function Spectra() {
   return (
@@ -8,6 +9,18 @@ function Spectra() {
       <Navbar />
 
       <h1>Spectra</h1>
+
+      <div className="Spectra-Trailer">
+        <iframe
+          width="1200"
+          height="675"
+          src="https://www.youtube.com/embed/C2L-XIdCrxU"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
 
       <Footer />
     </>
