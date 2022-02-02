97824869e35d5aae691572818c2e0ccdeec01bcf
bund798 <btech10123.20@bitmesra.ac.in>
2022-02-02 10:50:59 +0530

    added workshop poster

--- src/api/WorkshopInfo.js
+++ src/api/WorkshopInfo.js
@@ -1,4 +1,6 @@
-import Charcoal from "../assets/workshop posters/Charcoal.png";
+import Charcoal from "../assets/workshop posters/Charoal.jpg";
+import Coffee from "../assets/workshop posters/Coffee.jpg";
+import WaterColour from "../assets/workshop posters/WaterColour.jpg";
 
 const WorkshopInfo = [
   {
@@ -7,13 +9,13 @@ const WorkshopInfo = [
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
   },
   {
-    title: "Charcoal Workshop",
-    image: Charcoal,
+    title: "Coffee Workshop",
+    image: Coffee,
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
   },
   {
-    title: "Charcoal Workshop",
-    image: Charcoal,
+    title: "WaterColour Workshop",
+    image: WaterColour,
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
   },
 ];
