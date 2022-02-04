13a163fa6a52e05840ee450600f82a1354d773ee
Himanshu Murmu <himanshumurmu210@gmail.com>
2022-02-04 09:32:58 +0530

    updated slider and removed row2

--- src/assets/homepage gallery/index.js
+++ src/assets/homepage gallery/index.js
@@ -7,16 +7,6 @@ import A6 from "./row 1/img6.png";
 import A7 from "./row 1/img7.png";
 import A8 from "./row 1/img8.png";
 
-import B1 from "./row 2/img1.png";
-import B2 from "./row 2/img2.png";
-import B3 from "./row 2/img3.png";
-import B4 from "./row 2/img4.png";
-import B5 from "./row 2/img5.png";
-import B6 from "./row 2/img6.png";
-import B7 from "./row 2/img7.png";
-import B8 from "./row 2/img8.png";
-
 const Row1 = [A1, A2, A3, A4, A5, A6, A7, A8];
-const Row2 = [B1, B2, B3, B4, B5, B6, B7, B8];
 
-export { Row1, Row2 };
+export { Row1};
