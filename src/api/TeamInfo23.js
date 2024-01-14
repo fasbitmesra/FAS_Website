98cdef0200ef8882199d87e3dd1fd488f8b9f3c3
harsh <kumar.harshraj9@gmail.com>
2024-01-14 19:42:31 +0530

    updated exe body 24

---
+++ src/api/TeamInfo23.js
@@ -0,0 +1,262 @@
+
+// break point 
+import PrashantPrasad from "../assets/team pictures/PrashantPrasad.jpg";
+
+import Anukriti from "../assets/team pictures/Anukriti.jpg";
+import Meghana from "../assets/team pictures/Meghana.jpg";
+import Rooturaj from "../assets/team pictures/RooturajSwain.jpg";
+import DikshaPrasad from "../assets/team pictures/DikshaPrasad.jpg";
+import UrvashiAaryika from "../assets/team pictures/UrvashiAaryika.jpg";
+import HimanshuMurmu from "../assets/team pictures/HimanshuMurmu.jpg";
+import Utkarsh from "../assets/team pictures/Utkarsh.jpg";
+import ChiragJha from "../assets/team pictures/ChiragJha.jpg";
+import Sharad from "../assets/team pictures/Sharad.jpg";
+import Kaustav from "../assets/team pictures/Kaustav.jpg";
+import SouravJha from "../assets/team pictures/SouravJha.jpg";
+import Akshat from "../assets/team pictures/Akshat.jpg";
+import BibekRay from "../assets/team pictures/BibekRay.jpg";
+import Biswasarathi from "../assets/team pictures/Biswasarathi.jpg";
+import Avani from "../assets/team pictures/Avani.jpg";
+import Simrita from "../assets/team pictures/Simrita.jpg";
+import Shomya from "../assets/team pictures/Shomya.jpg";
+import PrashantPatil from "../assets/team pictures/PrashantPatil.jpg";
+import BundAgarwal from "../assets/team pictures/BundAgarwal.jpg";
+import Ritesh from "../assets/team pictures/Ritesh.jpg";
+import Avinash from "../assets/team pictures/Avinash.jpg";
+import Archana from "../assets/team pictures/Archana.jpg";
+import Anurag from "../assets/team pictures/Anurag.jpg";
+import Atreyee from "../assets/team pictures/Atreyee.jpg";
+import Samriddhi from "../assets/team pictures/Samriddhi.jpg";
+
+
+const TeamInfo23 = {
+  faculty: {
+    name: "Dr. Prashant Prasad",
+    post: "Faculty Advisor",
+    image: PrashantPrasad,
+  },
+
+
+  
+
+  members: [
+
+    
+    //break point for previous versions of website 
+    // k20
+    {
+      name: "Anukriti",
+      post: "President",
+      image: Anukriti,
+      facebookLink: "https://www.facebook.com/anukriti.chakrapani",
+      instagramLink: "https://instagram.com/anukriti945?igshid=YmMyMTA2M2Y=",
+      linkedinLink: "https://www.linkedin.com/in/anu-kriti-207ab2221",
+    },
+    {
+      name: "Meghana Tripathy",
+      post: "Joint President",
+      image: Meghana,
+      facebookLink: "/#team",
+      instagramLink: "https://instagram.com/meghana3779?igshid=YmMyMTA2M2Y=",
+      linkedinLink: "/#team",
+    },
+    {
+      name: "Diksha Prasad",
+      post: "Vice President",
+      image: DikshaPrasad,
+      facebookLink: "/#team",
+      instagramLink: "https://instagram.com/dksshaa?igshid=YmMyMTA2M2Y=",
+      linkedinLink: "https://www.linkedin.com/in/diksha-prasad-a30990199",
+    },
+    {
+      name: "Himanshu Murmu",
+      post: "Vice President",
+      image: HimanshuMurmu,
+      facebookLink: "https://www.facebook.com/profile.php?id=100006813677944",
+      instagramLink: "https://www.instagram.com/murmu.himanshu/",
+      linkedinLink: "https://www.linkedin.com/in/himanshu-murmu-5169061a5/",
+    },
+    {
+      name: "Utkarsh Kumar Singh",
+      post: "Vice President",
+      image: Utkarsh,
+      facebookLink: "/#team",
+      instagramLink: "https://www.instagram.com/singh_utkarsh902/",
+      linkedinLink:
+        "https://www.linkedin.com/in/utkarsh-kumar-singh-8a89581b6/",
+    },
+    {
+      name: "Chirag Jha",
+      post: "General Secretary",
+      image: ChiragJha,
+      facebookLink: "https://www.instagram.com/chiragjha_28/",
+      instagramLink: "https://www.instagram.com/chiragjha_28/",
+      linkedinLink: "https://www.linkedin.com/in/chirag-jha-a7894a220",
+    },
+    {
+      name: "Sharad Oraon",
+      post: "General Secretary ",
+      image: Sharad,
+      facebookLink: "https://www.facebook.com/wtfSharad/",
+      instagramLink: "https://www.instagram.com/wtfsharad/",
+      linkedinLink: "https://www.linkedin.com/in/sharad-oraon-a737431b5/",
+    },
+    {
+      name: "Kaustav Mitra",
+      post: "Joint Secretary",
+      image: Kaustav,
+      facebookLink: "https://www.facebook.com/profile.php?id=100006049156610",
+      instagramLink: "https://www.instagram.com/kaustav.mitra_/",
+      linkedinLink: "https://www.linkedin.com/in/kaustav-mitra-a64122205/",
+    },
+    {
+      name: "Sourav Jha",
+      post: "Treasurer",
+      image: SouravJha,
+      facebookLink: "https://m.facebook.com/100007048900769/",
+      instagramLink: "https://www.instagram.com/souravjha06/",
+      linkedinLink:
+        "https://www.linkedin.com/mwlite/in/sourav-kumar-jha-992004203",
+    },
+    {
+      name: "Akshat Sinha",
+      post: "Joint Treasurer",
+      image: Akshat,
+      facebookLink: "https://www.facebook.com/akshat.sinha.7965",
+      instagramLink: "https://www.instagram.com/akshat_sinha27/",
+      linkedinLink: "/#team",
+    },
+    {
+      name: "Bibek Ray",
+      post: "Design Head",
+      image: BibekRay,
+      facebookLink: "https://www.facebook.com/bibek.ray.9461",
+      instagramLink: "https://www.instagram.com/its_bibek.291",
+      linkedinLink: "https://www.linkedin.com/in/bibek-ray-061727220",
+    },
+    {
+      name: "Biswasarthi Basak",
+      post: "Content Head",
+      image: Biswasarathi,
+      facebookLink: "https://www.facebook.com/profile.php?id=100008261253504",
+      instagramLink: "https://www.instagram.com/biswasarathi_basak/",
+      linkedinLink: "https://www.linkedin.com/in/biswasarathi-basak-38159223a",
+    },
+    {
+      name: "Avani Jindal",
+      post: "Content Head",
+      image: Avani,
+      facebookLink: "/#team",
+      instagramLink: "/#team",
+      linkedinLink: "/#team",
+    },
+    {
+      name: "Prashant Sampatrao Patil",
+      post: "Social Media Head",
+      image: PrashantPatil,
+      facebookLink: "/#team",
+      instagramLink: "https://www.instagram.com/_prashant_7777/",
+      linkedinLink: "https://www.linkedin.com/in/prashant-patil-082255218",
+    },
+    {
+      name: "Shomya Chandrani",
+      post: "Social Media Head",
+      image: Shomya,
+      facebookLink: "https://www.facebook.com/shomya.chandrani",
+      instagramLink:
+        "https://instagram.com/shomya_chandrani?igshid=YmMyMTA2M2Y=",
+      linkedinLink: "/#team",
+    },
+    {
+      name: "Simrita Manda",
+      post: "Event Head",
+      image: Simrita,
+      facebookLink: "/#team",
+      instagramLink: "https://www.instagram.com/simrita365/",
+      linkedinLink: "https://www.linkedin.com/in/simrita-m-b37a22203",
+    },
+    {
+      name: "Bund Agarwal",
+      post: "Resource Head",
+      image: BundAgarwal,
+      facebookLink: "https://www.facebook.com/profile.php?id=100009267256050",
+      instagramLink:
+        "https://www.instagram.com/invites/contact/?i=u1s1k22ov6hr&utm_content=2hkpxoo",
+      linkedinLink: "https://www.linkedin.com/in/bund-agarwal-440369211",
+    },
+    {
+      name: "Ritesh Raj Sinha",
+      post: "Resource Head",
+      image: Ritesh,
+      facebookLink: "https://www.facebook.com/riteshrajsinha272",
+      instagramLink: "https://www.instagram.com/riteshrajsinha/",
+      linkedinLink: "https://www.linkedin.com/in/riteshrajsinha067/",
+    },
+    {
+      name: "Avinash Lakra",
+      post: "Publicity Head",
+      image: Avinash,
+      facebookLink: "https://www.facebook.com/avinash.lakra.338863",
+      instagramLink: "http://instagram.com/avinash_lakra21?utm_source=qr",
+      linkedinLink: "/#team",
+    },
+    {
+      name: "Archana Keshri",
+      post: "Publicity Head",
+      image: Archana,
+      facebookLink: "https://www.facebook.com/archana.keshri.9216",
+      instagramLink: "https://www.instagram.com/_.archana._.19/",
+      linkedinLink: "/#team",
+    },
+    {
+      name: "Anurag Pathank",
+      post: "Creattive Head",
+      image: Anurag,
+      facebookLink: "https://www.facebook.com/anurag.pathak.9026040",
+      instagramLink: "http://instagram.com/anu_rag0016",
+      linkedinLink: "https://www.linkedin.com/in/anurag-pathak-61677721a",
+    },
+    {
+      name: "Urvashi Aaryika",
+      post: "Executive Member",
+      image: UrvashiAaryika,
+      facebookLink: "/#team",
+      instagramLink: "https://www.instagram.com/urvashi0625/",
+      linkedinLink: "/#team",
+    },
+    {
+      name: "Atreyee Chatterjee",
+      post: "Executive Member",
+      image: Atreyee,
+      facebookLink: "https://www.facebook.com/rupa.chatterjee.798",
+      instagramLink: "https://instagram.com/mystique__ac?igshid=YmMyMTA2M2Y=",
+      linkedinLink: "/#team",
+    },
+    {
+      name: "Rooturaj Swain",
+      post: "Executive Member",
+      image: Rooturaj,
+      facebookLink: "/#team",
+      instagramLink: "/#team",
+      linkedinLink: "/#team",
+    },
+    {
+      name: "Samriddhi Kumari",
+      post: "Executive Member",
+      image: Samriddhi,
+      facebookLink: "/#team",
+      instagramLink: "/#team",
+      linkedinLink: "/#team",
+    },
+  ],
+
+
+
+
+
+
+
+  
+};
+
+export default TeamInfo23;
--- src/components/homepage/Team.js
+++ src/components/homepage/Team.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TeamInfo from "../../api/TeamInfo";
+import TeamInfo2 from "../../api/TeamInfo23";
 import { ReactComponent as TeamBG } from "../../assets/backgrounds/Team-bg.svg";
 import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
 import { BsInstagram } from "react-icons/bs";
@@ -17,9 +18,43 @@ function Team() {
           <p>{TeamInfo.faculty.name}</p>
           <p className="post">{TeamInfo.faculty.post}</p>
         </div>
+        <h2>Executive Body 2023-24</h2>
 
         <div className="Team-grid">
           {TeamInfo.members.map((member, index) => (
+            <div className="Member-card" key={member.name}>
+              <div className={`Member-pic col-${index % 5}`}>
+                <div className="Image-border">
+                  <img src={member.image} alt="..." />
+                </div>
+                <div className="Social-icon-container">
+                  <a href={member.facebookLink}>
+                    <div className="Social-icon">
+                      <FaFacebookF />
+                    </div>
+                  </a>
+                  <a href={member.instagramLink}>
+                    <div className="Social-icon">
+                      <BsInstagram />
+                    </div>
+                  </a>
+                  <a href={member.linkedinLink}>
+                    <div className="Social-icon">
+                      <FaLinkedinIn />
+                    </div>
+                  </a>
+                </div>
+                <TeamBG />
+              </div>
+              <p>{member.name}</p>
+              <p className="post">{member.post}</p>
+            </div>
+          ))}
+          
+        </div>
+        <h2>Executive Body 2022 -23</h2>
+        <div className="Team-grid">
+          {TeamInfo2.members.map((member, index) => (
             <div className="Member-card" key={member.name}>
               <div className={`Member-pic col-${index % 5}`}>
                 <div className="Image-border">
